e8310d8649a8860a093d1124c45936828b592fa4
root <root@localhost.localdomain>
2018-09-27 01:48:06 -0400

    upload camke train

---
+++ build/src/CMakeFiles/cmake_train.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmake_train.dir/cmake_train.cpp.o"
+  "../../bin/cmake_train.pdb"
+  "../../bin/cmake_train"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cmake_train.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
